aaf8d1ff96736d72d9999d6bbb9738a39a921de9
cartrugs <cartrugs@gmail.com>
2024-07-30 20:09:31 +0200

    Añadido de comentarios en conexion_comunidad

--- model/conexion_comunidad.php
+++ model/conexion_comunidad.php
@@ -1,15 +1,21 @@
 <?php
 function conectarBD() {
+    // Datos de conexión a la base de datos
     $host = 'localhost';
     $dbname = 'PruebaPractica';
     $username = 'root';
     $password = '';
 
     try {
+        // Crear una nueva instancia de PDO para la conexión a la base de datos
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        // Configurar el manejo de errores para que lance excepciones en caso de errores
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Devolver el objeto PDO para que pueda ser utilizado en otras partes del código
         return $pdo;
     } catch (PDOException $e) {
+        // Capturar cualquier excepción PDO y mostrar un mensaje de error
         die("Error en la conexión: " . $e->getMessage());
     }
 }
